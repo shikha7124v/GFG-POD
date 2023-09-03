a484fbfefdf239e97ab14587619493d3852b995f
shikha7124 <shikhaverma7124@gmail.com>
2023-09-03 22:14:12 +0530

    isIsomorphic

---
+++ Sep_2023/3sep-Is-Isomorphic.cpp
@@ -0,0 +1,19 @@
+// Check Whether Tree is Isomorphic
+/*
+bool isIsomorphic(Node *root1,Node *root2)
+    {
+    //add code here.
+        if(root1==NULL && root2==NULL)
+            return true;
+        if(root1==NULL || root2==NULL)
+            return false;
+        if(root1->data!=root2->data)
+            return false;
+            
+        bool a=isIsomorphic(root1->left,root2->left) && isIsomorphic(root1->right,root2->right);
+        
+        bool b=isIsomorphic(root1->left,root2->right) && isIsomorphic(root1->right,root2->left);
+        
+        return a||b;
+    }
+    */
